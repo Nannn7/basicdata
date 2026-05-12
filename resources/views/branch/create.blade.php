74608635b5cead22e559dfaca9fe20c598919d8e
Sholahuddin Al Ayubi <sholahuddin95@gmail.com>
2026-05-12 10:54:12 +0700

    feat: add create and edit views for core Basicdata modules

--- resources/views/branch/create.blade.php
+++ resources/views/branch/create.blade.php
@@ -30,8 +30,11 @@
                         Code
                     </label>
                     <div class="flex flex-wrap items-baseline w-full">
-                        <input class="input  @error('code') border-danger bg-danger-light @enderror" type="text"
-                            name="code" value="{{ $branch->code ?? '' }}">
+                        <input class="input @error('code') border-danger bg-danger-light @enderror" type="text"
+                            name="code" value="{{ $branch->code ?? '' }}" {{ isset($branch->id) ? 'readonly' : '' }}
+                            style="{{ isset($branch->id)
+                                ? 'background-color:#e5e7eb !important; color:#6b7280 !important; cursor:not-allowed !important; border-color:#d1d5db !important;'
+                                : '' }}">
                         @error('code')
                             <em class="alert text-danger text-sm">{{ $message }}</em>
                         @enderror
--- resources/views/developer/create.blade.php
+++ resources/views/developer/create.blade.php
@@ -31,7 +31,10 @@
                     </label>
                     <div class="flex flex-wrap items-baseline w-full">
                         <input class="input @error('code') border-danger bg-danger-light @enderror" type="text"
-                            name="code" value="{{ $developer->code ?? '' }}">
+                            name="code" value="{{ $developer->code ?? '' }}" {{ isset($developer->id) ? 'readonly' : '' }}
+                            style="{{ isset($developer->id)
+                                ? 'background-color:#e5e7eb !important; color:#6b7280 !important; cursor:not-allowed !important; border-color:#d1d5db !important;'
+                                : '' }}">
                         @error('code')
                             <em class="alert text-danger text-sm">{{ $message }}</em>
                         @enderror
