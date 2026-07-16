767ac4e13a91e61823c265c4c8e8c0876ecc1249
ramanda syah putra <ramanda.syah001@ag.co.id>
2026-07-16 13:45:26 +0700

    FIX : Menu Role

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,20 @@ use Illuminate\Support\Facades\Route;
 use Modules\Basicdata\Http\Controllers\BranchController;
 use Modules\Basicdata\Http\Controllers\CurrencyController;
 use Modules\Basicdata\Http\Controllers\HolidayCalendarController;
+use Modules\Basicdata\Http\Controllers\DeveloperController;
+use Modules\Basicdata\Http\Controllers\NotarisController;
+use Modules\Basicdata\Http\Controllers\MasterNamaDokumenController;
 
 /*
-    |--------------------------------------------------------------------------
-    | Web Routes
-    |--------------------------------------------------------------------------
-    |
-    | Here is where you can register web routes for your application. These
-    | routes are loaded by the RouteServiceProvider within a group which
-    | contains the "web" middleware group. Now create something great!
-    |
-    */
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::middleware(['auth'])->group(function () {
     Route::name('basicdata.')->prefix('basic-data')->group(function () {
@@ -60,5 +63,65 @@ Route::middleware(['auth'])->group(function () {
             Route::post('delete-multiple', [HolidayCalendarController::class, 'deleteMultiple'])->name('deleteMultiple');
         });
         Route::resource('holidaycalendar', HolidayCalendarController::class);
+
+        Route::name('developer.')->prefix('developer')->group(function () {
+            Route::get('datatables', [DeveloperController::class, 'dataForDatatables'])->name('datatables');
+            Route::get('export', [DeveloperController::class, 'export'])->name('export');
+            Route::post('delete-multiple', [DeveloperController::class, 'deleteMultiple'])->name('deleteMultiple');
+        });
+
+        Route::resource('developer', DeveloperController::class, [
+            'names' => [
+                'index' => 'developer.index',
+                'show' => 'developer.show',
+                'create' => 'developer.create',
+                'store' => 'developer.store',
+                'edit' => 'developer.edit',
+                'update' => 'developer.update',
+                'destroy' => 'developer.destroy',
+            ],
+        ]);
+
+        Route::name('notaris.')->prefix('notaris')->group(function () {
+            Route::get('datatables', [NotarisController::class, 'dataForDatatables'])->name('datatables');
+            Route::get('export', [NotarisController::class, 'export'])->name('export');
+            Route::post('delete-multiple', [NotarisController::class, 'deleteMultiple'])->name('deleteMultiple');
+        });
+
+        Route::resource('notaris', NotarisController::class, [
+            'names' => [
+                'index' => 'notaris.index',
+                'show' => 'notaris.show',
+                'create' => 'notaris.create',
+                'store' => 'notaris.store',
+                'edit' => 'notaris.edit',
+                'update' => 'notaris.update',
+                'destroy' => 'notaris.destroy',
+            ],
+            'parameters' => [
+                'notaris' => 'notaris',
+            ],
+        ]);
+
+        Route::name('master-nama-dokumen.')->prefix('master-nama-dokumen')->group(function () {
+            Route::get('datatables', [MasterNamaDokumenController::class, 'dataForDatatables'])->name('datatables');
+            Route::get('export', [MasterNamaDokumenController::class, 'export'])->name('export');
+            Route::post('delete-multiple', [MasterNamaDokumenController::class, 'deleteMultiple'])->name('deleteMultiple');
+        });
+
+        Route::resource('master-nama-dokumen', MasterNamaDokumenController::class, [
+            'names' => [
+                'index' => 'master-nama-dokumen.index',
+                'show' => 'master-nama-dokumen.show',
+                'create' => 'master-nama-dokumen.create',
+                'store' => 'master-nama-dokumen.store',
+                'edit' => 'master-nama-dokumen.edit',
+                'update' => 'master-nama-dokumen.update',
+                'destroy' => 'master-nama-dokumen.destroy',
+            ],
+            'parameters' => [
+                'master-nama-dokumen' => 'masterNamaDokumen',
+            ],
+        ]);
     });
 });
